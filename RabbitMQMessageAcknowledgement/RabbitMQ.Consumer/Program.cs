d7c278b087167b2c001772d4a915a51ffbb49b83
Enes AKTAŞ <105974395+enesaktas10@users.noreply.github.com>
2024-08-06 14:51:57 +0300

    Add files via upload

---
+++ RabbitMQMessageAcknowledgement/RabbitMQ.Consumer/Program.cs
@@ -0,0 +1,28 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+//Create connection
+ConnectionFactory factory = new();
+factory.Uri = new Uri("amqps://ddmxhagm:7jYQOH-lm2u1Hm3-EoVlwQEsftiuIkLt@chimpanzee.rmq.cloudamqp.com/ddmxhagm");
+
+//Activating the connection and opening a channel
+using IConnection connection = factory.CreateConnection();
+using IModel channel = connection.CreateModel();
+
+//Queuing
+channel.QueueDeclare(queue: "example-queue", exclusive: false);
+
+//Creating consumer
+EventingBasicConsumer consumer = new(channel);
+
+//Reading message from queue
+channel.BasicConsume(queue: "example-queue", autoAck: false, consumer: consumer);
+consumer.Received += (sender, e) =>
+{
+    Console.WriteLine(Encoding.UTF8.GetString(e.Body.Span));
+    channel.BasicAck(deliveryTag:e.DeliveryTag,multiple:false);
+};
+
+
+Console.Read();
---
+++ RabbitMQMessageAcknowledgement/RabbitMQ.Publisher/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+
+//Create connection
+ConnectionFactory factory = new();
+factory.Uri = new Uri("amqps://ddmxhagm:7jYQOH-lm2u1Hm3-EoVlwQEsftiuIkLt@chimpanzee.rmq.cloudamqp.com/ddmxhagm");
+
+//Activating the connection and opening a channel
+using IConnection connection =factory.CreateConnection();
+using IModel channel = connection.CreateModel();
+
+//Queuing
+channel.QueueDeclare(queue: "example-queue",exclusive:false);
+
+
+
+//Send a message to the queue
+for (int i = 0; i < 100; i++)
+{
+    await Task.Delay(200);
+    byte[] message = Encoding.UTF8.GetBytes("Hello World" + i);
+    channel.BasicPublish(exchange:"",routingKey:"example-queue",body:message);
+}
+
+Console.Read();
